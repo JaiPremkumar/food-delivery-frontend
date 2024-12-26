764fd0c9f44e6e3a46eaa789e449df37d8941c95
JaiPremkumar <prem.jai411@gmail.com>
2024-12-26 17:41:45 +0530

    all i commite

--- src/component/action/kitchenAction.js
+++ src/component/action/kitchenAction.js
@@ -8,7 +8,7 @@ export const getKitchen = async(dispatch)=>{
         dispatch(kitchensRequest())
        
         const {data} = await axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent('https://backend-food-delivery-1.onrender.com/api/v1/kitchen')}`) 
-        dispatch(kitchensSuccess(data))
+        dispatch(kitchensSuccess(data.contents.kitchens))
         console.log(data) 
     } catch (error) {
         dispatch(kitchensFailed(error.response.data.message)) 
