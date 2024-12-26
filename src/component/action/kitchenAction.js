47d4575f03a1cff3d0b5c02bc350c34733aecff2
JaiPremkumar <prem.jai411@gmail.com>
2024-12-26 21:22:36 +0530

    my commited

--- src/component/action/kitchenAction.js
+++ src/component/action/kitchenAction.js
@@ -6,10 +6,10 @@ import { kitchensFailed, kitchensRequest, kitchensSuccess } from "../slices/Kitc
 export const getKitchen = async(dispatch)=>{
     try {
         dispatch(kitchensRequest())
-       
-        const {response} = await axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent('https://backend-food-delivery-1.onrender.com/api/v1/kitchen')}`) 
-        dispatch(kitchensSuccess(response))
-        console.log(response.contents.kitchens) 
+       // `https://api.allorigins.win/get?url=${encodeURIComponent('https://backend-food-delivery-1.onrender.com/api/v1/kitchen')}`
+        const {data} = await axios.get('https://backend-food-delivery-1.onrender.com/api/v1/kitchen') 
+        dispatch(kitchensSuccess(data))
+        console.log(data) 
     } catch (error) {
         dispatch(kitchensFailed(error.response.data.message)) 
     }
